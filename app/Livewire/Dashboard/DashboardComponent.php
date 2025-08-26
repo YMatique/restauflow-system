f46acd067341f653725ee3254cc8b5c591db9c08
Yuvi Matique <86082620+YMatique@users.noreply.github.com>
2025-08-26 15:29:10 +0200

    creating Dashboard component

---
+++ app/Livewire/Dashboard/DashboardComponent.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Livewire\Dashboard;
+
+use Livewire\Component;
+
+class DashboardComponent extends Component
+{
+     protected string $layout = 'layouts.app';
+    public function render()
+    {
+        return view('livewire.dashboard.dashboard-component', [
+            'title' => 'Dashboard Principal',
+            'breadcrumb' => 'Dashboard'
+        ]);
+    }
+}
