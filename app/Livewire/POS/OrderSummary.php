4042d646ec740e6857fbac3f0474c7b2244ce76e
Yuvi Matique <86082620+YMatique@users.noreply.github.com>
2025-09-19 08:42:37 +0200

    creating partials files for pos

---
+++ app/Livewire/POS/OrderSummary.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\POS;
+
+use Livewire\Component;
+
+class OrderSummary extends Component
+{
+    public function render()
+    {
+        return view('livewire..p-o-s.order-summary');
+    }
+}
