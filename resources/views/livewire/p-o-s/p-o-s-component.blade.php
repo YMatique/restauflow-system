7d584687805579919586460da97b528269de4314
Yuvi Matique <86082620+YMatique@users.noreply.github.com>
2025-09-25 23:42:18 +0200

    severals updates

--- resources/views/livewire/p-o-s/p-o-s-component.blade.php
+++ resources/views/livewire/p-o-s/p-o-s-component.blade.php
@@ -1,5 +1,6 @@
 {{-- resources/views/livewire/p-o-s/p-o-s-component.blade.php --}}
 <div class="flex overflow-hidden h-full">
+
     <!-- Sidebar Esquerdo -->
     {{-- <x-slot name="sidebar"> --}}
         <div class="w-80 bg-white dark:bg-zinc-800 border border-zinc-200 dark:border-zinc-700 rounded-xl mr-4 flex flex-col overflow-hidden">
